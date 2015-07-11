aed49e97496e3634d6a448d60246901c1d855165
unknown <linhnv1@101LinhNV1.fsoft.fpt.vn>
2015-07-11 14:36:39 +0700

    Init ok

--- init.lua
+++ init.lua
@@ -1,14 +1,30 @@
 print("Set wifi to STATION mode")
 wifi.setmode(wifi.STATION)
 wifi.sta.autoconnect(1)
-print("Set 5s alarm timer to check wifi")
-tmr.alarm(0, 5000, 0, function ()
-	if (wifi.sta.status() == 5) then
-		print("Wifi is connected, IP is " .. wifi.sta.getip())
+function smartok (ssid, pass)
+	print("Smartconfig successed, id is [" .. ssid .."] and pass [" .. pass .."]")
+	wifi.sta.config(ssid, pass, 1)
+	wifi.stopsmart()
+end
+insmart=0
+checkInteval=5000
+ssid, password, bssid_set, bssid=wifi.sta.getconfig()
+if ( ssid ~= "" ) then
+checkInteval=15000
+end
+print("Set " .. (checkInteval/1000) .. "s alarm timer to check wifi")
+tmr.alarm(0, checkInteval, 1, function ()
+	if (insmart == 0) then
+		if (wifi.sta.status() == 5) then
+			print("Wifi is connected, IP is " .. wifi.sta.getip())
+			tmr.stop(0)
+		else
+			print("Wifi is not connected, start smartconfig")
+			wifi.setmode(wifi.STATION)
+			insmart=1
+			wifi.startsmart(0, smartok)
+			end
 	else
-		print("Wifi is not connected, start smartconfig")
-		wifi.startsmart(0, function ()
-			print("Start config successed!!!")
-			end )
-		end
+		print("In smart config...")
+	end
 	end )
